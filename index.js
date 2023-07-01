4ab3cde1c6d598ea334d10b02d28105506c72f35
bogdanstojkovski <bogdanstojkovski@gmail.com>
2023-07-01 09:34:34 +0200

    Update

--- index.js
+++ index.js
@@ -10,24 +10,23 @@ const chatId = '5272022550';
 let lastRecordTime = null;
 let lastYuliaValue = null;
 
-const bot = new TelegramBot(telegramToken, {polling: true});
+const bot = new TelegramBot(telegramToken, { polling: true });
 
 setInterval(async () => {
-  const response = await axios.get(`https://api.airtable.com/v0/${airtableBase}/${airtableTable}`, {
-    headers: {
-      Authorization: `Bearer ${airtableApiKey}`
-    },
-    params: {
-      sort: [{ field: 'createdTime', direction: 'desc' }],
-      maxRecords: 1
-    }
-  });
+  try {
+    const response = await axios.get(`https://api.airtable.com/v0/${airtableBase}/${airtableTable}`, {
+      headers: {
+        Authorization: `Bearer ${airtableApiKey}`,
+      },
+      params: {
+        sort: [{ field: 'createdTime', direction: 'desc' }],
+        maxRecords: 1,
+      },
+    });
 
-  const record = response.data.records[0];
-
-
+    const record = response.data.records[0];
     const yuliaCheckbox = record.fields.Yulia;
-    
+
     // If the Yulia checkbox has changed from false to true
     if (yuliaCheckbox && !lastYuliaValue) {
       lastYuliaValue = yuliaCheckbox;
@@ -45,5 +44,7 @@ setInterval(async () => {
     } else {
       lastYuliaValue = yuliaCheckbox;
     }
-  
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }, 10000);
